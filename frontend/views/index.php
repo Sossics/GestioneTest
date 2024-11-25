dc9145e99d924b72f7496d5f18160d35a637abaf
Sossics <michelerusso267@gmail.com>
2024-11-25 18:44:05 +0100

    Update index.php

---
+++ frontend/views/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dashboard</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <?php include("components/head.php"); ?>
+    </head>
+    <body style="background-color: rgb(240, 235, 248);">
+        <?php include("components/navbar.php")?>
+        <div class="container mt-5">
+            <h1 class="text-center">Benvenuto, <?php echo htmlspecialchars($_SESSION['user']['nome']); ?>!</h1>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
