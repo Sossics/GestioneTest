8fdd4bc2e305167388ad1eef4399c346695363b1
Sossics <michelerusso267@gmail.com>
2024-11-28 05:40:35 +0100

    Delete script1.sql

--- backend/SQL/script1.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `tentativo` CHANGE `data_tentativo` `data_tentativo` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE `sessione` CHANGE `data_inizio` `data_inizio` DATETIME NOT NULL, CHANGE `data_fine` `data_fine` DATETIME NOT NULL;
