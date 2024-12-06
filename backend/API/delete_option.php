e631ec484fd0d0564412a5465b0c30596c795c20
Sossics <michelerusso267@gmail.com>
2024-12-06 14:40:16 +0100

    Added APi for deleting question optioni

---
+++ backend/API/delete_option.php
@@ -0,0 +1,33 @@
+<?php
+
+$logDir = __DIR__ . "/logs/delete_option";
+if (!is_dir($logDir)) {
+    mkdir($logDir, 0777, true);
+}
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/logs/delete_option/php_error.log');
+header('Content-Type: application/json');
+$f = fopen($logDir . "/log.txt", "a+");
+
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    include './../include/db_connect.php';
+
+    $input = json_decode(file_get_contents('php://input'), true);
+
+    $option_id = intval($input['id']);
+    $stmt = $conn->prepare('DELETE FROM opzioni_domanda WHERE id=?');
+    $stmt->bind_param('i', $option_id);
+    if($stmt->execute()){
+        echo json_encode(['success' => true, 'error' => null]);
+    }else{
+        echo json_encode(['success' => false, 'error' => 'SQL error']);
+    }
+
+} else {
+    echo json_encode(['success' => false, 'error' => 'Invalid request method']);
+}
