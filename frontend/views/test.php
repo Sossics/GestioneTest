c2c9076c84dcddf06d33ea095c0df5b02820d156
Sossics <michelerusso267@gmail.com>
2024-12-18 02:27:38 +0100

    Test is now saving the answers through a POST form

--- frontend/views/test.php
+++ frontend/views/test.php
@@ -1,9 +1,39 @@
 <?php
 
-session_start();
 var_export($_POST);
+
+define('SECRET_KEY', 'g78gh789f4328h79g890aafzxvssfga72gfas');
+
+function decryptId($encrypted) {
+    $method = 'AES-256-CBC';
+    $key = hash('sha256', SECRET_KEY, true);
+    list($encryptedData, $iv) = explode('::', base64_decode($encrypted), 2);
+    $iv = base64_decode($iv);
+    $id = openssl_decrypt($encryptedData, $method, $key, 0, $iv);
+    return $id;
+}
+
+session_start();
 require("./../../backend/Include/db_connect.php");
 $filter = isset($_GET['filter']) ? trim($_GET['filter']) : '';
+
+if(isset($_POST['attempt_code'])){
+    $SQL_check_attempt_existance = "SELECT
+                                        id AS attempt
+                                    FROM
+                                        tentativo
+                                    WHERE
+                                        id=?";
+    $stmt_check_attempt_existance = $conn->prepare($SQL_check_attempt_existance);
+    $decrypted_attempt_id = decryptId($_POST['attempt_code']);
+    $stmt_check_attempt_existance->bind_param("i", $decrypted_attempt_id);
+    $stmt_check_attempt_existance->execute();
+    $result_check_attempt_existance = $stmt_check_attempt_existance->get_result();
+    if($result_check_attempt_existance && $result_check_attempt_existance->num_rows > 0){
+        unset($_POST['attempt_code']);
+    }
+}
+
 switch($_SESSION['user']['ruolo']){
     case "STUDENTE":
             $SQL_query = "SELECT
@@ -197,7 +227,7 @@ switch($_SESSION['user']['ruolo']){
                 </nav>
             <?php endif; ?>
         </div>
-        <div class="d-flex <?= isset($_POST['attempt_code']) ? "justify-content-center" : "justify-content-between" ?> align-items-center mb-3">
+        <div class="d-flex flex-column <?= isset($_POST['attempt_code']) ? "justify-content-center" : "justify-content-between" ?> align-items-center mb-3">
             <?php if (isset($_POST['id'])): ?>
                 <?php if (isset($_POST['attempt_code'])): ?>
                     <h2 class="text-center"><?= $row['test_titolo'] ?></h2>
@@ -212,84 +242,14 @@ switch($_SESSION['user']['ruolo']){
                     <button type="submit" class="btn btn-primary">Cerca</button>
                 </form>
             <?php endif; ?>
+            <h3 class="text-center m-0">
+                <div class="spinner-border" role="status" style="display: none;"></div>
+            </h3>
+            <p id="saving_message" style="display: none;"></p>
         </div>
         <?php
         if (isset($_POST['id'])) {
-            if(isset($_POST['attempt_code'])){
-
-                $test_id = isset($_POST['id']) ? (int) $_POST['id'] : 0;
-
-                $SQL_query_test = "SELECT t.titolo, d.nome AS nome_docente, d.cognome AS cognome_docente
-                                    FROM test AS t
-                                    JOIN utente AS d ON d.codice_fiscale = t.cf_docente
-                                    WHERE t.id = ?";
-                $stmt_test = $conn->prepare($SQL_query_test);
-                $stmt_test->bind_param("i", $test_id);
-                $stmt_test->execute();
-                $result_test = $stmt_test->get_result();
-
-                if ($result_test->num_rows == 0) {
-                    echo "Test non trovato!";
-                    exit();
-                }
-
-                $row_test = $result_test->fetch_assoc();
-
-                // Domande fetch SQL
-                $SQL_query_domande = "SELECT id, testo, tipo
-                                    FROM domanda
-                                    WHERE test_id = ?";
-                $stmt_domande = $conn->prepare($SQL_query_domande);
-                $stmt_domande->bind_param("i", $test_id);
-                $stmt_domande->execute();
-                $result_domande = $stmt_domande->get_result();
-
-                echo '<form id="test-form">';
-                    if ($result_domande->num_rows > 0) {
-                        while ($row_domanda = $result_domande->fetch_assoc()) {
-                            // print_r($row_domanda);
-                            echo "<div class='question-container'>";
-                            
-                            // Testo
-                            echo "<p class='question-title'>" . htmlspecialchars($row_domanda['testo']) . "</p>";
-
-                            // tipo "APERTA"
-                            if ($row_domanda['tipo'] == 'APERTA') {
-                                echo "<div class='question-text'>
-                                        <label for='answer_{$row_domanda['id']}'>Risposta:</label>
-                                        <textarea id='answer_{$row_domanda['id']}' class='form-control' rows='4' placeholder='Scrivi la tua risposta'></textarea>
-                                    </div>";
-                            }
-
-                            // tipo "SCELTA_MULTIPLA"
-                            if ($row_domanda['tipo'] == 'MULTIPLA') {
-                                // Opzioni
-                                $SQL_query_opzioni = "SELECT * FROM opzioni_domanda WHERE domanda_id = ?";
-                                $stmt_opzioni = $conn->prepare($SQL_query_opzioni);
-                                $stmt_opzioni->bind_param("i", $row_domanda['id']);
-                                $stmt_opzioni->execute();
-                                $result_opzioni = $stmt_opzioni->get_result();
-                                
-                                echo "<div class='options-list'>";
-                                while ($row_opzione = $result_opzioni->fetch_assoc()) {
-                                    // print_r($row_opzione);
-                                    echo "<div class='form-check'>
-                                            <input class='form-check-input' type='radio' name='question_{$row_domanda['id']}' id='option_{$row_opzione['id']}'>
-                                            <label class='form-check-label' for='option_{$row_opzione['id']}'>
-                                                " . $row_opzione['testo_opzione'] . "
-                                            </label>
-                                        </div>";
-                                }
-                                echo "</div>";
-                            }
-                            echo "</div>";
-                        }
-                    } else {
-                        echo "<p>Non ci sono domande per questo test.</p>";
-                    }
-                echo '</form>';
-            }else{
-                $SQL_query_fetch_tries = "SELECT
+            $SQL_query_fetch_tries = "SELECT
                                             COUNT(t.id) AS tentativi_usati,
                                             s.svolgibile AS test_status,
                                             s.id AS session_id
@@ -307,6 +267,89 @@ switch($_SESSION['user']['ruolo']){
                 $result_fetch_tries = $stmt_fetch_tries->get_result();
                 $row_fetch_tries = $result_fetch_tries->fetch_assoc();
                 // print_r($row_fetch_tries);
+            if(isset($_POST['attempt_code'])){
+                if(isset($_POST['data_attempt'])){
+                    // var_export($_POST['data_attempt']);
+                }else{
+                    $test_id = isset($_POST['id']) ? (int) $_POST['id'] : 0;
+    
+                    $SQL_query_test = "SELECT t.titolo, d.nome AS nome_docente, d.cognome AS cognome_docente
+                                        FROM test AS t
+                                        JOIN utente AS d ON d.codice_fiscale = t.cf_docente
+                                        WHERE t.id = ?";
+                    $stmt_test = $conn->prepare($SQL_query_test);
+                    $stmt_test->bind_param("i", $test_id);
+                    $stmt_test->execute();
+                    $result_test = $stmt_test->get_result();
+    
+                    if ($result_test->num_rows == 0) {
+                        echo "Test non trovato!";
+                        exit();
+                    }
+    
+                    $row_test = $result_test->fetch_assoc();
+    
+                    // Domande fetch SQL
+                    $SQL_query_domande = "SELECT id, testo, tipo
+                                        FROM domanda
+                                        WHERE test_id = ?";
+                    $stmt_domande = $conn->prepare($SQL_query_domande);
+                    $stmt_domande->bind_param("i", $test_id);
+                    $stmt_domande->execute();
+                    $result_domande = $stmt_domande->get_result();
+    
+                    echo "<h3 class='text-center'>
+                            <div class='spinner-border' role='status' style='display: none;'></div>
+                        </h3>";
+                    echo '<form id="test-form" method="POST">';
+                    echo '<input type="hidden" name="id" value="'.$_POST['id'].'">';
+                    echo '<input type="hidden" name="attempt_code" value="'.$_POST['attempt_code'].'">';
+                        if ($result_domande->num_rows > 0) {
+                            while ($row_domanda = $result_domande->fetch_assoc()) {
+                                // print_r($row_domanda);
+                                echo "<div class='question-container'>";
+                                
+                                // Testo
+                                echo "<p class='question-title'>" . htmlspecialchars($row_domanda['testo']) . "</p>";
+    
+                                // tipo "APERTA"
+                                if ($row_domanda['tipo'] == 'APERTA') {
+                                    echo "<div class='question-text'>
+                                            <label for='answer_{$row_domanda['id']}'>Risposta:</label>
+                                            <textarea id='answer_{$row_domanda['id']}' name='answer_{$row_domanda['id']}' class='form-control' rows='4' placeholder='Scrivi la tua risposta'></textarea>
+                                        </div>";
+                                }
+    
+                                // tipo "SCELTA_MULTIPLA"
+                                if ($row_domanda['tipo'] == 'MULTIPLA') {
+                                    // Opzioni
+                                    $SQL_query_opzioni = "SELECT * FROM opzioni_domanda WHERE domanda_id = ?";
+                                    $stmt_opzioni = $conn->prepare($SQL_query_opzioni);
+                                    $stmt_opzioni->bind_param("i", $row_domanda['id']);
+                                    $stmt_opzioni->execute();
+                                    $result_opzioni = $stmt_opzioni->get_result();
+                                    
+                                    echo "<div class='options-list'>";
+                                    while ($row_opzione = $result_opzioni->fetch_assoc()) {
+                                        // print_r($row_opzione);
+                                        echo "<div class='form-check'>
+                                                <input class='form-check-input' type='radio' name='question_{$row_domanda['id']}_{$row_opzione['id']}' id='option_{$row_opzione['id']}'>
+                                                <label class='form-check-label' for='option_{$row_opzione['id']}'>
+                                                    " . $row_opzione['testo_opzione'] . "
+                                                </label>
+                                            </div>";
+                                    }
+                                    echo "</div>";
+                                }
+                                echo "</div>";
+                            }
+                        } else {
+                            echo "<p>Non ci sono domande per questo test.</p>";
+                        }
+                    echo "<input type=\"submit\" value=\"Invia Tentativo\" name=\"data_attempt\">";
+                    echo '</form>';
+                }
+            }else{
     
                 echo "<table class='table table-bordered w-50'>";
                 echo "<tbody>
@@ -326,7 +369,10 @@ switch($_SESSION['user']['ruolo']){
                         ";
     
                 $SQL_query_attempt_details = "SELECT 
-                        t.data_tentativo AS inviato_il
+                        t.data_tentativo AS inviato_il,
+                        s.max_tentativi_ammessi AS max_attempts,
+                        s.visibilita_tentativi AS attempts_visibility,
+                        COUNT(t.id) AS count_attempts
                       FROM 
                         tentativo AS t
                       JOIN 
@@ -340,15 +386,15 @@ switch($_SESSION['user']['ruolo']){
                       WHERE 
                         s.test_id = ?
                       AND
-                        stu.codice_fiscale=?
-                      AND
-                        s.visibilita_tentativi=1";
+                        stu.codice_fiscale=?";
     
                 $stmt_attempt_details = $conn->prepare($SQL_query_attempt_details);
                 $stmt_attempt_details->bind_param("is", $_POST['id'], $_SESSION['user']['codice_fiscale']);
                 $stmt_attempt_details->execute();
                 $result_attempt_details = $stmt_attempt_details->get_result();
-    
+                $max_attempts = null;
+                $num_attempts = null;
+                $visibile = null;
                 echo "<form action='visualizza_tentativo.php' method='POST'><table class='table table-bordered w-50 mt-4'>";
                 echo "<thead>
                         <tr>
@@ -365,6 +411,11 @@ switch($_SESSION['user']['ruolo']){
     
                 if ($result_attempt_details->num_rows > 0) {
                     while ($row_attempt = $result_attempt_details->fetch_assoc()) {
+                        if(!isset($max_attempts, $num_attempts, $visibile)){
+                            $max_attempts = $row_attempt['max_attempts'];
+                            $num_attempts = $row_attempt['count_attempts'];
+                            $visibile = $row_attempt['attempts_visibility'];
+                        }
                         echo "<tr class='text-center'>";
                         echo "<td>" . htmlspecialchars($row_attempt['inviato_il']) . "</td>";
                         // echo "<td>" . htmlspecialchars($row_attempt['punteggio']) . "</td>";
@@ -375,9 +426,9 @@ switch($_SESSION['user']['ruolo']){
                                 <input type='hidden' name='attempt_id' value='" . $row_attempt['inviato_il'] . "'>
                                 <input type='hidden' name='session_id' value='" . $row_fetch_tries['session_id'] . "'>
                 
-                                <button class='btn btn-link show-answers' type='submit'>
+                                ".(($visibile) ? "<button class='btn btn-link show-answers' type='submit'>
                                     Visualizza Risposte
-                                </button>
+                                </button>" : "Non Visibile"). "
                              </td>";
                         echo "</tr>";
                     }
@@ -388,7 +439,7 @@ switch($_SESSION['user']['ruolo']){
                 echo "</tbody>";
                 echo "</table></form>";
                 // var_export($row_fetch_tries);
-                if($row_fetch_tries['test_status'] == 1){
+                if($row_fetch_tries['test_status'] == 1 && $num_attempts < $max_attempts){
                     echo "<button class='btn btn-success fs-6' onclick='startAttempt();'>Avvia un Tentativo</button>";
                 }
             }
@@ -499,6 +550,57 @@ switch($_SESSION['user']['ruolo']){
             document.body.append(form);
             form.submit();
         }
+
+        document.addEventListener('DOMContentLoaded', () => {
+            const student_form = document.getElementById('test-form');
+            const spinner = document.querySelector('.spinner-border');
+            const message = document.getElementById('saving_message');
+
+            if (student_form) {
+                student_form.addEventListener('submit', async (e) => {
+                    e.preventDefault();
+
+                    spinner.style.display = 'block';
+                    message.style.display = 'block';
+                    message.innerHTML = 'Salvataggio...';
+                    student_form.style.display = 'none';
+
+                    const formData = new FormData(student_form);
+                    const jsonData = Object.fromEntries(formData.entries());
+
+                    try {
+                        const response = await fetch('../../backend/API/save_answers.php', {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify({
+                                studentID: "<?= $_SESSION['user']['codice_fiscale'] ?? ''?>",
+                                testID: "<?= $_POST['id'] ?? ''?>",
+                                attemptID: "<?= $_POST['attempt_code'] ?? ''?>",
+                                data: jsonData
+                            })
+                        });
+
+                        if (response.ok) {
+                            const result = await response.json();
+                            console.log('Success:', result);
+
+                            message.innerHTML = `<p class="text-success">Risposte inviate con successo!</p>`;
+                            setTimeout(() => {
+                                window.location.reload();
+                            }, 1500);
+                        } else {
+                            console.error('Errore durante l\'invio:', response.statusText);
+                            message.innerHTML = `<p class="text-danger">Errore durante l'invio delle risposte.</p>`;
+                        }
+                    } catch (error) {
+                        console.error('Fetch error:', error);
+                        message.innerHTML = `<p class="text-danger">Errore di rete. Controlla la connessione.</p>`;
+                    }
+                });
+            }
+        });
 
     </script>
 </body>
